830235724eb8d8e8d2fccb7b039e87f6b02dcab9
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2026-02-24 10:22:50 +0100

    Convert rust UTF-8 strings to ISO-8859-1 before passing to vhpi_printf

--- vhpi/src/lib.rs
+++ vhpi/src/lib.rs
@@ -28,7 +28,20 @@ use std::ffi::CString;
 extern crate num_derive;
 
 pub fn printf(msg: impl AsRef<str>) {
-    let cstr = CString::new(msg.as_ref()).unwrap();
+    // Convert UTF-8 string to ISO-8859-1 bytes
+    let iso8859_1_bytes: Vec<u8> = msg
+        .as_ref()
+        .chars()
+        .map(|c| {
+            let code = c as u32;
+            if code <= 0xFF {
+                code as u8
+            } else {
+                b'?' // Replace characters outside ISO-8859-1 range
+            }
+        })
+        .collect();
+    let cstr = CString::new(iso8859_1_bytes).unwrap();
     static FMT: &[u8] = b"%s\n\0";
     unsafe { vhpi_sys::vhpi_printf(FMT.as_ptr().cast::<i8>(), cstr.as_ptr()) };
 }
